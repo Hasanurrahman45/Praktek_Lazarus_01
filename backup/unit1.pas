14736f2583b3ea69b8f819213a3c564294c63924
hasanurrahman <rahman.hasanur45@gmail.com>
2020-04-17 20:22:56 +0700

    membuat showmessage Hai sayang

---
+++ backup/unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    eNama: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure eNamaChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  showMessage ('Hai '+eNama.text);
+end;
+
+procedure TForm1.eNamaChange(Sender: TObject);
+begin
+
+end;
+
+end.
+
